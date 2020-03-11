d5c6a49ce2eda28fbf00c04efa687a02d061d96e
Sunny Hebbar <sunny@jbcole.co.uk>
2020-03-11 14:15:10 +0000

    Refactored views to use REST framework's api_view decorator instead

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from tutorial import views
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+# Append a set of format_suffix_patterns to the existing URLs - works with the format pattern specified in our views
+# We don't necessarily need to add these extra url patterns in, but it gives us a simple, clean way of referring to
+# a specific format
+urlpatterns = format_suffix_patterns(urlpatterns)
--- tutorial/views.py
+++ tutorial/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from tutorial.models import Snippet
 from tutorial.serializers import SnippetSerializer
 
 
 # Create your views here.
-@csrf_exempt
-def snippet_list(request):
+# This decoratpr provides a few bits of functionality such as making sure you receive Request instances in your view,
+# and adding context to Response objects so that content negotiation can be performed
+# It also provides behaviour such as returning 405 Method Not Allowed responses when appropriate, and handling any
+# ParseError exceptions that occur when accessing request.data with malformed input.
+@api_view(['GET', 'POST'])
+# Using format suffixes gives us URLs that explicitly refer to a given format, and means our API will be able to
+# handle URLs
+def snippet_list(request, format=None):
     """
         List all code snippets, or create a new snippet.
         """
@@ -18,25 +24,27 @@ def snippet_list(request):
         snippets = Snippet.objects.all()
         # Serialize the snippets using many=True to specify multiple snippets
         serializer = SnippetSerializer(snippets, many=True)
-        # Return them as a JSON response
-        return JsonResponse(serializer.data, safe=False)
+        # Return the serialized snippets - the api_view decorator allows us to use Response instead of JsonResponse
+        # It automatically retrieves the response in the relevant data format
+        return Response(serializer.data)
 
     # CREATE
     elif request.method == 'POST':
-        # Deserialize the request
-        data = JSONParser().parse(request)
+        # Deserialize the request - we don't need to use this when using the @api_view decorator
+        # data = JSONParser().parse(request)
         # Restore the native datatype into a fully populated object instance
-        serializer = SnippetSerializer(data=data)
+        serializer = SnippetSerializer(data=request.data)
         # Save the object instance if it's valid
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
+            # Instead of specifying the actional status code, we use the REST framework's named status codes
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         # Return an error if it isn't valid
-        return JsonResponse(serializer.errors, status=400)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-def snippet_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk, format=None):
     """
     Retrieve, update or delete a code snippet.
     """
@@ -44,29 +52,29 @@ def snippet_detail(request, pk):
         # Get the snippet by the pk
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
-        # Return error if it doesn't exist
-        return HttpResponse(status=404)
+        # Return error if it doesn't exist - we don't need to use HttpResponse, as api_view handles it for us
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     # RETRIEVE
     if request.method == 'GET':
         # Serialize the snippet and return it as a JSON response if we want to retrieve it
         serializer = SnippetSerializer(snippet)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     # UPDATE
     elif request.method == 'PUT':
-        # Deserialize the request
-        data = JSONParser().parse(request)
+        # Deserialize the request - again, we don't need to do this, as api_view handles it for us
+        # data = JSONParser().parse(request)
         # Restore the native datatype into a fully populated object instance
-        serializer = SnippetSerializer(snippet, data=data)
+        serializer = SnippetSerializer(snippet, data=request.data)
         # Save the object instance if it's valid
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
+            return Response(serializer.data)
         # Return an error if it isn't valid
-        return JsonResponse(serializer.errors, status=400)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     # DELETE
     elif request.method == 'DELETE':
         snippet.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
