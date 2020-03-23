3a304433f6b819be8ddb2684a04bfbdbfe62dabb
Sunny Hebbar <sunny@jbcole.co.uk>
2020-03-23 14:30:14 +0000

    Amended Snippet model to enable snippet highlighting based on owner

--- tutorial/models.py
+++ tutorial/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
+# Allows us to populate the highlighted field when the model is saved
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 
 
 # Create your models here.
@@ -19,6 +23,23 @@ class Snippet(models.Model):
     lineons = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+    # Used to represent the user who created the snippet
+    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
+    # Used to store the HTML representation of the code snippet
+    highlighted = models.TextField()
 
     class Meta:
         ordering = ['created']
+
+    def save(self, *args, **kwargs):
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        lineons = 'table' if self.lineons else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, lineons=lineons,
+                                  full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
